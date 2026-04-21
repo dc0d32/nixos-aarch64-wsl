ece1ba51103e54ebb35b96be6187c6168fab2a2c
_ _ <6890606+dc0d32@users.noreply.github.com>
2026-04-20 17:08:33 -0700

    Enhance Zsh config with completion and dircolors
    Added completion styling and dircolors integration to Zsh configuration.

--- home.nix
+++ home.nix
@@ -9,6 +9,27 @@
     autocd = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+    enableCompletion = true;
+    initContent = ''
+      # Completion Styling
+      zstyle ':completion:*' auto-description 'specify: %d'
+      zstyle ':completion:*' completer _expand _complete _correct _approximate
+      zstyle ':completion:*' format 'Completing %d'
+      zstyle ':completion:*' group-name '''
+      zstyle ':completion:*' menu select=2
+
+      # Integration with dircolors
+      # Note: Home Manager's dircolors module will set LS_COLORS
+      zstyle ':completion:*:default' list-colors ''${(s.:.)LS_COLORS}
+
+      zstyle ':completion:*' list-colors '''
+      zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+      zstyle ':completion:*' matcher-list ''' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
+      zstyle ':completion:*' menu select=long
+      zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+      zstyle ':completion:*' use-compctl false
+      zstyle ':completion:*' verbose true
+    '';
     history = {
       size = 50000;
       save = 50000;
@@ -24,6 +45,12 @@
       gl = "git log --oneline -20";
       nr = "sudo nixos-rebuild switch --flake ~/nix#$(if [ \"$(uname -m)\" = aarch64 ]; then echo aarch64; else echo x86_64; fi)";
     };
+  };
+
+  # Ensures dircolors is installed and configured
+  programs.dircolors = {
+    enable = true;
+    enableZshIntegration = true;
   };
 
   # --- starship prompt ---
